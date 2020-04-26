547c61120dc284a6073a85420dd9dd0636b44917
Роман Данилевич <dwordchanel@gmail.com>
2020-04-27 01:13:54 +0300

    Add project files.

---
+++ OOP_lab_3_15_1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace OOP_lab_3_15_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = Console.ReadLine();
+
+            str = str.ToLower();
+
+            string[] words = str.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            Console.WriteLine();
+
+            foreach (string word in words)
+            {
+                if (word.Trim(new char[] { ' ', ':', ';', '.', ',', '?', '!', '(', ')', '{', '}', '[', ']', '@', '#', '№', '$', '^', '%', '&', '*' }).Length % 2 == 0)
+                {
+                    Console.WriteLine(word.Trim(new char[] { ' ', ':', ';', '.', ',', '?', '!', '(', ')', '{', '}', '[', ']', '@', '#', '№', '$', '^', '%', '&', '*' }));
+                }
+            }    
+        }
+    }
+}
